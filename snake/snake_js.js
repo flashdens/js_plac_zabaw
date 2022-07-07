c727938ab6d46894c9a4f3ddaef6114458858df0
flashdens <metis234@gmail.com>
2022-07-07 19:43:18 +0200

    w większości działa

--- snake/snake_js.js
+++ snake/snake_js.js
@@ -6,7 +6,6 @@ const kolorWeza = 'red';
 const kolorObwodu = 'black';
 const wartosci = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200,
     220, 240, 260, 280, 300, 320, 340, 360, 380];
-const wynikDisplay = document.getElementById("wynik");
 let wonsz = [
     { x: 180, y: 200 }, // [0] - glowa
     { x: 200, y: 200 }, // [1]
@@ -14,12 +13,12 @@ let wonsz = [
 ];
 let owoc = [];
 const predkosc = 20;
-const fps = 5;
+const fps = 10;
 let glowa = wonsz[0];
 let kierunek = 1;
 let owocNaPlanszy = false;
 let graTrwa = true;
-let wynik = 0
+let wynik = (wonsz.length) - 4;
 let px = 10;
 let py = 0;
 
@@ -53,13 +52,12 @@ function czyPorazka() {
     for (let i = 1; i < wonsz.length; i++)
         if ((glowa.x === wonsz[i].x && glowa.y === wonsz[i].y))
             czyZderzenie = true;
-    if (glowa.x < 0 || glowa.x > 400 || glowa.y < 0 || glowa.y > 400 || czyZderzenie === true) {
+    if (glowa.x < 0 || glowa.x > 399 || glowa.y < 0 || glowa.y > 399 || czyZderzenie === true) {
         graTrwa = false;
         alert("Przegrałeś! Twój wynik to: " + `${wonsz.length - 4}` + ".");
     }
 }
 
-// nowa plansza ładuje się z każdą klatką, więc starą trzeba wyczyścić
 function wyczyscPlansze() {
     rysowanko.fillStyle = kolorTla;
     rysowanko.strokestyle = obwodPlanszy;
@@ -67,7 +65,6 @@ function wyczyscPlansze() {
     rysowanko.strokeRect(0, 0, plansza.width, plansza.height);
 }
 
-// funkcja rysująca jedno ciałko węża
 function narysujCialko(wonsz) {
     rysowanko.fillStyle = kolorWeza;
     rysowanko.strokestyle = kolorObwodu;
@@ -75,34 +72,37 @@ function narysujCialko(wonsz) {
     rysowanko.strokeRect(wonsz.x, wonsz.y, 20, 20);
 }
 
-// funkcja rysująca owoc
 function narysujOwoc() {
     rysowanko.fillStyle = kolorWeza;
     rysowanko.fillRect(owoc.x, owoc.y, 20, 20);
     rysowanko.strokestyle = kolorObwodu;
     rysowanko.strokeRect(owoc.x, owoc.y, 20, 20);
 }
-// funkcja rysująca węża
 function narysujWeza() {
-    wonsz.forEach(narysujCialko)
+    wonsz.forEach(narysujCialko);
 }
 
-// funkcja robiąca owoc
+function losujXYOwocu() {
+    owoc.x = wartosci[Math.floor(Math.random() * wartosci.length)];
+    owoc.y = wartosci[Math.floor(Math.random() * wartosci.length)];
+}
+
 function czyZrobicOwoc() {
-    if (owocNaPlanszy === false) {
-        owoc.x = wartosci[Math.floor(Math.random() * wartosci.length)];
-        owoc.y = wartosci[Math.floor(Math.random() * wartosci.length)];
+    if (!owocNaPlanszy) {
+        losujXYOwocu();
+        for (let i = 0; i < wonsz.length; i++)
+            if ((owoc.x === wonsz[i].x) && (owoc.y === wonsz[i].y))
+                losujXYOwocu();
         owocNaPlanszy = true;
     }
 }
 
-// funkcja sprawia, że wąż rośnie
 function rosniecie() {
     if (glowa.x === owoc.x && glowa.y === owoc.y) {
         owocNaPlanszy = false;
     }
 }
-// w zależności od kierunku następuje ruch (wtf???)
+
 function ruchWeza() {
     switch (kierunek) {
         case 0:
@@ -128,11 +128,6 @@ function ruchWeza() {
         wonsz.length--;
 }
 
-function aktualizujDane() {
-
-}
-
-// główna funkcja
 function klatka() {
     czyPorazka();
     ruchWeza();
@@ -141,11 +136,9 @@ function klatka() {
     czyZrobicOwoc();
     narysujOwoc();
     rosniecie();
-    console.log(wonsz.length)
 };
 
 
-// wywołanie głównej funkcji, z odpowiednim timeoutem
 setInterval(function () {
     if (graTrwa) {
         klatka();
